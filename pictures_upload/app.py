3514cdcfafb60164b0d242ea43bdd8f0f95ea7e4
kanzaki <prpr@mail.com>
2025-05-02 14:17:33 +0900

    pictureView

--- pictures_upload/app.py
+++ pictures_upload/app.py
@@ -1,4 +1,13 @@
 # ローカルにファイルを保存するFlaskアプリ（雛形）
+# ローカルに保存したファイルを一覧表示させる
+# image_gallery/
+# ├── app.py
+# ├── static/ flask は static配下に画像を入れる必要がある
+# │   └── images/ 
+# ├── templates/
+# │   ├── upload.html
+# │   └── gallery.html
+#
 
 from flask import Flask, request, render_template, redirect, url_for, flash
 import os
@@ -12,9 +21,12 @@ app.secret_key = "secret" # flash用
 # アプリ（Pythonファイル）と同じ階層にuploadフォルダが作成されます。
 os.makedirs(config.UPLOAD_FOLDER, exist_ok=True)
 
-# ブラウザで / （トップページ）にアクセスしたとき、この関数が動きます。
+def alowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in config.ALLOWED_EXTENSIONS
+
+# アップロードページ
 # GETリクエスト（ページを表示するとき）とPOSTリクエスト（ファイル送信時）の両方に対応。
-@app.route("/", methods=["GET", "POST"])
+@app.route("/upload", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
         if "file" not in request.files:
@@ -35,7 +47,21 @@ def upload_file():
             file.save(save_path)
             flash(f"{filename}をローカルに保存しました")
 
-        return redirect(url_for("upload_file"))
+        return redirect(url_for("gallery"))
 
     return render_template("upload.html")
+
+# 画像一覧ページ
+@app.route("/gallery")
+def gallery():
+    if config.USE_S3:
+        # S3一覧取得処理（後ほど記述）
+        image_urls = []
+    else:
+        files = os.listdir(config.UPLOAD_FOLDER)
+        image_urls = [url_for('static', filename=f"images/{f}") for f in files if alowed_file(f)]
+    return render_template("gallery.html", images=image_urls)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
--- pictures_upload/config.py
+++ pictures_upload/config.py
@@ -1,4 +1,5 @@
 # 設定で保存先を切り替えるようにする
 
 USE_S3 = False
-UPLOAD_FOLDER = "./uploads" # ローカル保存先
+UPLOAD_FOLDER = "./static/images" # ローカル保存先
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
